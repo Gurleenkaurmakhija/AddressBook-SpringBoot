109d076c6d6082b8d1c5e88e84786752da9ca5ed
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-02-07 23:08:35 +0530

    [GurleenKaur]: add custom address book exception

---
+++ src/main/java/com/gevernova/addressbook/exception/AddressBookException.java
@@ -0,0 +1,7 @@
+package com.gevernova.addressbook.exception;
+
+public class AddressBookException extends RuntimeException {
+    public AddressBookException(String message) {
+        super(message);
+    }
+}
